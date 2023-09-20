361780666ca7a618e26a7dd5a866d32279114868
fongkei320 <92391007+fongkei320@users.noreply.github.com>
2023-09-20 17:01:39 -0400

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { TextInputAutocompleteModule } from "angular-text-input-autocomplete";
 import { CustomMenuComponent } from "./custom-menu.component";
+import {NgxTributeModule} from 'ngx-tribute';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, TextInputAutocompleteModule],
+  imports: [BrowserModule, FormsModule, TextInputAutocompleteModule, NgxTributeModule],
   declarations: [AppComponent, CustomMenuComponent],
   bootstrap: [AppComponent]
 })
